c09afe9d9b289b8d09af3b14a7a7997b06b13f37
Mitra Simman <mitra.simman@meyicloud.com>
2025-07-05 17:47:27 +0530

    Initial React frontend commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Hello from AWS CodePipeline!</h1>
+      <p>This is a simple React app deployed from GitHub to S3.</p>
+    </div>
+  );
+}
+
+export default App;
